7b2f99f35657dda24b7b8e498efad51c81a7a9b1
ritesh choudhary <riteshchoudharyind@gmail.com>
2024-05-06 18:24:25 +0530

    Add test for handling single string parameter

--- spec/string_calculator_spec.rb
+++ spec/string_calculator_spec.rb
@@ -5,7 +5,11 @@ describe StringCalculator do
 
   describe "#add" do
     it 'returns 0 for an empty string' do
-      expect(string_calculator.add("")).to eq(0)
+    	expect(string_calculator.add("")).to eq(0)
+    end
+
+    it 'returns the number for single parameter' do
+      expect(string_calculator.add("1")).to eq(1)
     end
   end
 end
